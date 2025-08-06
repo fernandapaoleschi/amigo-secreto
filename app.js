515733eaea38a82a7d3d0ef43dbfa70db2bd33e7
Fernanda Paoleschi <fernandapaoleschi1@gmail.com>
2025-08-06 18:16:24 -0300

    fix: alert caso usuário clique em adicionar sem digitar um nome antes

--- app.js
+++ app.js
@@ -2,33 +2,39 @@ const listaDeAmigos = [];
 const inputNome = document.getElementById('amigo');
 const inputNumero = document.querySelector("#amigo");
 
-inputNumero.addEventListener("keypress", function(e){
-  const keyCode = (e.keyCode ? e.keyCode : e.wich);
-  if (keyCode > 47 && keyCode < 58){
-    e.preventDefault();
-  }
+inputNumero.addEventListener("keypress", function (e) {
+    const keyCode = (e.keyCode ? e.keyCode : e.wich);
+    if (keyCode > 47 && keyCode < 58) {
+        e.preventDefault();
+    }
 })
 
 
 function adicionarAmigo() {
-  const nomeTexto = inputNome.value.trim();
-  listaDeAmigos.push(nomeTexto);
-  atualizarLista();
-  inputNome.value = '';
+
+    const nomeTexto = inputNome.value.trim();
+    if (nomeTexto === '') {
+        alert('Por favor, digite o nome de um amigo antes de adicionar.');
+        return;
+    }
+    listaDeAmigos.push(nomeTexto);
+    atualizarLista();
+    inputNome.value = '';
+
 }
 
-function atualizarLista(){
-  const listaNome = document.getElementById('listaAmigos');
-  listaNome.innerHTML = '';
+function atualizarLista() {
+    const listaNome = document.getElementById('listaAmigos');
+    listaNome.innerHTML = '';
 
-  listaDeAmigos.forEach(nomeTexto => {
-    const li = document.createElement('li');
-    li.textContent = nomeTexto;
-    listaNome.appendChild(li);
-  })
+    listaDeAmigos.forEach(nomeTexto => {
+        const li = document.createElement('li');
+        li.textContent = nomeTexto;
+        listaNome.appendChild(li);
+    })
 }
 
-function sortearAmigo(){
+function sortearAmigo() {
     const indiceAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
     const nomeSorteado = listaDeAmigos[indiceAleatorio];
     document.getElementById('resultado').textContent = `Nome sorteado: ${nomeSorteado}`;
